72357c7053d22d17b7484571e7323d1d42c47464
Nigorjeanluc <nigorjeanluc@gmail.com>
2018-01-21 12:40:48 +0200

    ad1

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -183,6 +183,18 @@
 									</div>
 								@endforeach
 								</div>
+
+								<div class="col-md-12">
+									<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+										<!-- Ad 1 -->
+										<ins class="adsbygoogle"
+											style="display:inline-block;width:300px;height:600px"
+											data-ad-client="ca-pub-1410948886873143"
+											data-ad-slot="3346196762"></ins>
+										<script>
+										(adsbygoogle = window.adsbygoogle || []).push({});
+									</script>
+								</div>
 							</div>
 						</div>
 					</div>
