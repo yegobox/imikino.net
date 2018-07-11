8f33796df72c7cf4365ff9cd3f069dc14017f1ee
Nigorjeanluc <nigorjeanluc@gmail.com>
2018-07-11 12:37:06 +0200

    Face2

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -2,7 +2,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 	<meta name="author" content="Igor Jean-Luc Ndiramiye">
-	<meta property="fb:pages" content="331259140367167" />
+	<meta property="fb:pages" content="286094315130272" />
 	
     <title>@yield('title') | Imikino.net</title>
 	
