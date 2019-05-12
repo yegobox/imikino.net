3151dcc13637ee2be150aed4d6db35084669dace
Nigorjeanluc <nigorjeanluc@gmail.com>
2019-05-12 13:57:17 +0200

    ChatFacebook

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,13 +5,31 @@
 </head>
 
 <body>
+        <!-- Load Facebook SDK for JavaScript -->
+        <div id="fb-root"></div>
+        <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v3.3'
+          });
+        };
+        
+        (function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
+        
         <!-- Your customer chat code -->
         <div class="fb-customerchat"
-          attribution=setup_tool
+          attribution=install_email
           page_id="286094315130272"
           theme_color="#13cf13"
-          logged_in_greeting="Chat with Imikino.net now"
-          logged_out_greeting="Chat with Imikino.net now">
+            logged_in_greeting="Chat with Imikino.net now"
+            logged_out_greeting="Chat with Imikino.net now">
         </div>
     {{--  <div id="chat">
         <header class="baslik">
--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -22,24 +22,6 @@
 	<link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css"  type="text/css">
 	
 	<script src="https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js"></script>
-	<!-- Load Facebook SDK for JavaScript -->
-<div id="fb-root"></div>
-<script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      xfbml            : true,
-      version          : 'v3.3'
-    });
-  };
-
-  (function(d, s, id) {
-	var js, fjs = d.getElementsByTagName(s)[0];
-	if (d.getElementById(id)) return;
-	js = d.createElement(s); js.id = id;
-	js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-	fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-</script>
 
 
 	
