25e236a23fb9e6d43a86dcecee4365b3554b4228
Nigorjeanluc <nigorjeanluc@gmail.com>
2019-08-10 16:19:46 +0200

    SocialNu

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -55,9 +55,9 @@
 				<li><a href="/videos">Videos</a></li>
 			</ul>
 			<ul class="list-inline navbar-right top-social">
-				<li><a href="https://www.facebook.com/imikino.net/" target="_blank" rel="nofollow"><i class="fa fa-facebook"></i></a></li>
-				<li><a href="https://twitter.com/ImikinoCom" target="_blank" rel="nofollow"><i class="fa fa-twitter"></i></a></li>
-				<li><a href="https://www.instagram.com/imikino.net3/"><i class="fa fa-instagram"></i></a></li>
+				<li><a href="https://www.facebook.com/imikino.rw" target="_blank" rel="nofollow"><i class="fa fa-facebook"></i></a></li>
+				<li><a href="#" target="_blank" rel="nofollow"><i class="fa fa-twitter"></i></a></li>
+				<li><a href="https://www.instagram.com/imikino.rw/"><i class="fa fa-instagram"></i></a></li>
 				<li><a href="#"><i class="fa fa-google-plus-square"></i></a></li>
 				<li><a href="#"><i class="fa fa-whatsapp"></i></a></li>
 				<li><a href="{{ route('journalist.login') }}" target="_blank"><i class="fa fa-users"></i></a></li>
